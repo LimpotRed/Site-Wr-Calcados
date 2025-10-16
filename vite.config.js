57fdaa4925f6459f97720441a717d9534607c399
Lucas Persijn <lucaslopesbastos221@gmail.com>
2025-10-16 15:53:25 -0300

    dominio

--- vite.config.js
+++ vite.config.js
@@ -202,7 +202,7 @@ logger.error = (msg, options) => {
 }
 
 export default defineConfig({
-	base: '/Site-Wr-Calcados/',
+	base: './',
 	customLogger: logger,
 	plugins: [
 		...(isDev ? [inlineEditPlugin(), editModeDevPlugin(), iframeRouteRestorationPlugin()] : []),
